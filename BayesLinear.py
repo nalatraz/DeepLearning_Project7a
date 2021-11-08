d95a6efdf5471264abdb0a7937d3108589cf4953
zartalan <90852557+zartalan@users.noreply.github.com>
2021-11-08 10:44:35 +0100

    Bayes Linear Module Uploaded

---
+++ BayesLinear.py
@@ -0,0 +1,47 @@
+import math
+
+import torch
+from torch.nn import Module, Parameter
+import torch.nn.init as init
+import torch.nn.functional as F
+
+class BayesLinear(Module):
+
+    def __init__(self, prior_mu, prior_sigma, in_features, out_features, bias=True):
+        super(BayesLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        
+        self.prior_mu = prior_mu
+        self.prior_sigma = prior_sigma
+        self.prior_log_sigma = math.log(prior_sigma)
+        
+        self.weight_mu = Parameter(torch.Tensor(out_features, in_features))
+        self.weight_log_sigma = Parameter(torch.Tensor(out_features, in_features))
+        self.register_buffer('weight_eps', None)
+
+        self.bias_mu = Parameter(torch.Tensor(out_features))
+        self.bias_log_sigma = Parameter(torch.Tensor(out_features))
+        self.register_buffer('bias_eps', None)
+
+   def forward(self, input):
+        
+        if self.weight_eps is None :
+            weight = self.weight_mu + torch.exp(self.weight_log_sigma) * torch.randn_like(self.weight_log_sigma)
+        else :
+            weight = self.weight_mu + torch.exp(self.weight_log_sigma) * self.weight_eps
+        
+        if self.bias:
+            if self.bias_eps is None :
+                bias = self.bias_mu + torch.exp(self.bias_log_sigma) * torch.randn_like(self.bias_log_sigma)
+            else :
+                bias = self.bias_mu + torch.exp(self.bias_log_sigma) * self.bias_eps                
+        else :
+            bias = None
+            
+        return F.linear(input, weight, bias)
+
+    def extra_repr(self):
+        
+        return 'prior_mu={}, prior_sigma={}, in_features={}, out_features={}, bias={}'.format(self.prior_mu, self.prior_sigma, self.in_features, self.out_features, self.bias is not None)
+
